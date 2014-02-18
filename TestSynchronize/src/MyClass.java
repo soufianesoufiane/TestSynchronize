fabbd267ba46377d51439c94943857866e749155
soufianesoufiane <laanibi.soufiane@gmail.com>
2014-02-18 14:27:47 +0000

    create a second class

---
+++ TestSynchronize/src/MyClass.java
@@ -0,0 +1,13 @@
+
+public class MyClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the second class i've create");
+
+	}
+
+}
