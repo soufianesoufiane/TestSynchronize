eae8d93535a14effaa697f99237d93148d73e38a
soufianesoufiane <laanibi.soufiane@gmail.com>
2014-02-18 12:22:52 +0000

    change file

--- TestSynchronize/src/MainApp.java
+++ TestSynchronize/src/MainApp.java
@@ -7,7 +7,8 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello From Soufiane & Hiba !!!");
-
+		System.out.println("Are u here ?");
+		System.out.println("The third Change");
+		System.out.println("The Fouth Change");
 	}
-
 }
